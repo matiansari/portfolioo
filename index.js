768cf6071e7013d2d19679f3d275719f76caf014
matiansari <ansarimati277@gmail.com>
2023-05-18 20:41:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// Auto Typed Name
+// let typed = new Typed(".auto-typed", {
+//     strings: ["Wasim Rehman,"],
+//     typeSpeed: 220,
+//     backSpeed: 220,
+//     loop: true,
+// });
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+//
+
+var swiper = new Swiper(".swiper-container1", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+var swiper = new Swiper(".swiper-container2", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+var swiper = new Swiper(".swiper-container3", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
